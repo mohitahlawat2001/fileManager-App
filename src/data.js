2f24faef0ceca66f41723d1cca9fa70ff5fdc98d
Mohit ahlawat <65100859+mohitahlawat2001@users.noreply.github.com>
2023-06-13 14:29:05 +0530

    added files in public and data .js in src

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const data = [
+    {
+      id: 1,
+      name: 'Video File',
+      type: 'video',
+      path: "/file-server/video.mp4"
+    },
+    {
+      id: 2,
+      name: 'Audio File',
+      type: 'audio',
+      path: "/file-server/audio.mp3"
+    },
+    {
+      id: 3,
+      name: 'Document File',
+      type: 'document',
+      path: "/file-server/document.pdf"
+    },
+    {
+      id: 4,
+      name: 'Image File',
+      type: 'image',
+      path: "/file-server/image1.png"
+  },
+   {
+    id: 5,
+    name: 'Image File',
+    type: 'image',
+    path: "/file-server/image2.png"
+    }
+  ]
